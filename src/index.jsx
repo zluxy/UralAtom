fb775215be8ebfce0e564fc0af3d1a467f65ea4c
Ruslan <48005934+zluxy@users.noreply.github.com>
2023-06-26 20:52:59 +0000

    Update 10 files

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import ReactDOMClient from "react-dom/client";
 import { UralAtomMainPage } from "./screens/UralAtomMainPage";
+import { ONasPage } from "./screens/ONasPage";
+import { KatalogProduksii } from "./screens/KatalogProduksii";
+import { Kontaktnaya } from "./screens/Kontaktnaya";
+import { ZayavkaPage } from "./screens/ZayavkaPage";
+import { NovostiPage } from "./screens/NovostiPage";
 
 const app = document.getElementById("app");
 const root = ReactDOMClient.createRoot(app);
-root.render(<UralAtomMainPage />);
+root.render(<NovostiPage />);
---
+++ src/screens/NovostiPage/index.js
@@ -0,0 +1,1 @@
+export { NovostiPage } from "./NovostiPage";
